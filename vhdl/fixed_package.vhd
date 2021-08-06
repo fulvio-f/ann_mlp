02cecce7ab0fa90df83b4d22bd9c89bcf42ab091
JoaoEvangelista99 <82846973+JoaoEvangelista99@users.noreply.github.com>
2021-08-06 20:20:22 -0300

    Correção de erros

--- vhdl/fixed_package.vhd
+++ vhdl/fixed_package.vhd
@@ -1,9 +1,4 @@
---Alunos: Fulvio Favilla Filho		  - 11366893
---	  João Antônio Evangelista Garcia - 11234231
---        Guilherme Lopes Matias          - 11234102
-
---			  
--- Pacote de operações em Ponto Fixo
+-- Pacote de operacoes em Ponto Fixo
 ----------------------------------------------------------------------------------------------------
 
 PACKAGE fixed_package IS
@@ -18,10 +13,10 @@ PACKAGE fixed_package IS
     FUNCTION to_integer (arg_L: fixed) RETURN INTEGER;
     FUNCTION "+"(arg_L, arg_R: fixed) RETURN FIXED;
     FUNCTION "+"(arg_L: INTEGER; arg_R: FIXED) RETURN FIXED;
-    FUNCTION "+"(arg_L: fixed; arg_R: INTEGER) RETURN INTEGER;
+    FUNCTION "+"(arg_L: fixed; arg_R: INTEGER) RETURN FIXED;
     FUNCTION "-"(arg_L, arg_R: fixed) RETURN FIXED;
     FUNCTION "*"(arg_L, arg_R: FIXED) RETURN FIXED;
-    FUNCTION to_fixed(arg_L:REAL; max_range, min_range:fixed_range) RETURN FIXED;
+    FUNCTION to_fixed (arg_L: real; max_range, min_range: fixed_range) RETURN FIXED;
     FUNCTION to_real (arg_L:FIXED) RETURN real;
     FUNCTION "+"(arg_L: FIXED; arg_R: REAL) RETURN FIXED;
     FUNCTION "+"(arg_L: REAL; arg_R: FIXED) RETURN FIXED;
@@ -29,17 +24,22 @@ PACKAGE fixed_package IS
     FUNCTION "-"(arg_L: REAL; arg_R: FIXED) RETURN FIXED;
     FUNCTION "*"(arg_L: FIXED; arg_R: REAL) RETURN FIXED;
     FUNCTION "*"(arg_L: REAL; arg_R: FIXED) RETURN FIXED;
+    FUNCTION MAXIMUM(arg_L, arg_R: integer) RETURN INTEGER;
+    FUNCTION MINIMUM(arg_L, arg_R: integer) RETURN INTEGER;
+    FUNCTION COMP1_FIXED(arg_L: FIXED) RETURN FIXED;
+    FUNCTION ADD_SUB_FIXED(arg_L, arg_R: FIXED; c: bit) RETURN FIXED;
+    function MULT_FIXED (arg_L, arg_R: fixed) return fixed;
 
 END fixed_package;
 
 --Corpo do pacote
 PACKAGE BODY fixed_package IS
 
-	--Declaração das funções auxiliares do pacote fixed_package
+	--DeclaraÃ§Ã£o das funcoes auxiliares do pacote fixed_package
 --------------------------------------------------------------
 	--Retorna o maior valor entre dois argumentos
 
-	FUNCTION MAX(arg_L, arg_R: integer) RETURN INTEGER IS
+	FUNCTION MAXIMUM(arg_L, arg_R: integer) RETURN INTEGER IS
 		VARIABLE maior : INTEGER;
 	BEGIN
 		IF arg_L > arg_R THEN
@@ -49,11 +49,11 @@ PACKAGE BODY fixed_package IS
 		END IF;
 		
 		RETURN maior;
-	END MAX;
+	END MAXIMUM;
 --------------------------------------------------------------
 	--Retorna o menor valor entre dois argumentos
 
-	FUNCTION MIN(arg_L, arg_R: integer) RETURN INTEGER IS
+	FUNCTION MINIMUM(arg_L, arg_R: integer) RETURN INTEGER IS
 		VARIABLE menor : INTEGER;
 	BEGIN
 		IF arg_L < arg_R THEN
@@ -63,7 +63,7 @@ PACKAGE BODY fixed_package IS
 		END IF;
 		
 		RETURN menor;
-	END MIN;
+	END MINIMUM;
 --------------------------------------------------------------
 	--Retorna o complemento de 1 do argumento
 
@@ -95,82 +95,94 @@ PACKAGE BODY fixed_package IS
 --------------------------------------------------------------
 	--Retorna o produto entre dois argumentos
 
-	FUNCTION MULT_FIXED(arg_L, arg_R: FIXED) RETURN FIXED IS
-	
-		--Dimensões das matrizes
-		CONSTANT m : INTEGER := arg_L'LENGTH;
-		CONSTANT n : INTEGER := arg_R'LENGTH;
+	function MULT_FIXED (arg_L, arg_R: fixed) return fixed is
+		--Os valores de m e n sao obtidos dos sinais a e b a serem multiplicados.
+		constant m: integer := arg_L'length;		--Eh sempre igual a 32 para o projeto
+		constant n: integer := arg_R'length;		--Eh sempre igual a 32 para o projeto
+		alias arg_Lv : fixed(m-1 downto 0) is arg_L;
+		alias arg_Rv : fixed(n-1 downto 0) is arg_R;
 		
-		--Definindo um tipo matriz
-		TYPE matrix IS ARRAY (NATURAL RANGE <>, NATURAL RANGE <>) OF BIT;
-	
-		--Matrizes utilizadas no cálculo
-		VARIABLE Mij : matrix(0 TO m-1, 0 TO m+n-1);
-		VARIABLE Cij : matrix(0 TO m-1, 0 TO m+n);
-		VARIABLE Pij : matrix(0 TO m, 0 TO m+n);
-	
-		VARIABLE arg_R_linha : FIXED(m+n-1 DOWNTO 0);
-		VARIABLE P : FIXED(m+n-1 DOWNTO 0);		--Produto final
-	
-	BEGIN
-		
-		arg_R_linha := (m+n-1 DOWNTO n => '0') & arg_R;
-		
-		
-		INIT_Cij: FOR i IN 0 TO m-1 LOOP	  --Cij(i,0) = 0
-			Cij(i,0) := '0';
-		END LOOP INIT_Cij;
-		
-		
-		INIT_Pij_coluna: FOR i IN 0 TO m LOOP    --Pij(i,0) = 0
-			Pij(i,0) := '0';
-		END LOOP INIT_Pij_coluna;
-	
-	
-		INIT_Pij_linha: FOR j IN 1 TO m+n-1 LOOP    --Pij(m,j) = 0
-			Pij(m,j) := '0';
-		END LOOP INIT_Pij_linha;
-	
-	
-		Mij_coluna: FOR i IN m-1 DOWNTO 0 LOOP
-		
-			Mij_linha: FOR j IN m+n-1 DOWNTO 0 LOOP
+		variable RESULT: fixed(arg_L'RANGE);
 			
-				Mij(i,j) := arg_L(i) AND arg_R_linha(j);
-				
-			END LOOP Mij_linha;
-			
-		END LOOP Mij_coluna;
-			
-			
-		Pij_coluna: FOR i IN m-1 DOWNTO 0 LOOP
-		
-			Pij_linha: FOR j IN m+n-1 DOWNTO 0 LOOP
-			
-				Pij(i,j+1) := Pij(i+1,j) XOR Mij(i,j) XOR Cij(i,j);
-				
-				Cij(i,j+1) := (Pij(i+1,j) AND (Mij(i,j) OR Cij(i,j))) OR (Mij(i,j) AND Cij(i,j));
-				
-			END LOOP Pij_linha;
-			
-		END LOOP Pij_coluna;
-		
+		-- Variaveis para armazenamento dos dados de entrada e de saida no formato bit_vector
+		variable a, b, p: bit_vector(m+n-1 downto 0);
 	
-	
-		P_final: FOR i IN m+n-1 DOWNTO 0 LOOP		--Obtendo os valores para o produto P
-			P(i) := Pij(0,i+1);
-		END LOOP P_final;
-	
-	
-		RETURN P;
-	
-	END MULT_FIXED;
+		--Primeiro criamos um tipo ?matrix?
+		type matrix is array (natural range <>, natural range <>) of bit;
+
+		--Depois criamos as matrizes de interconexao:
+		--Mi,j	Mij(i, j)		0 <= i <= m+n-1, 0 <= j <= m+n-1
+		variable Mij: matrix(0 to m+n-1, 0 to m+n-1);
+		--Ci,j	Cij(i, j)		0 <= i <= m+n-1, 0 <= j <= m+n
+		variable Cij: matrix (m+n-1 downto 0, m+n downto 0);
+		--Pi,j	Pij(i, j) 	0 <= i <= m+n, 0 <= j <= m+n
+		variable Pij: matrix (m+n downto 0, m+n downto 0);
+
+	begin
+		--Transferencia dos valores de arg_Rv para Lv e Rv, respectivamente:
+		transf_a: for i in arg_Lv'range loop
+			a(i) := arg_Lv(i);
+		end loop;
+		transf_b: for i in arg_Rv'range loop
+			b(i) := arg_Rv(i);
+		end loop;
+
+		--Extensao dos bits de sinal em a e b:
+		a(m+n-1 downto m) := (others => arg_Lv(arg_Lv'left));	--(others => arg_Lv(arg_Lv'LEFT));		-- blinha(m+n-1 downto n => b('left)); para signed
+		b(m+n-1 downto n) := (others => arg_Rv(arg_Rv'left));	--(others => arg_Rv(arg_Rv'LEFT));		-- alinha(m+n-1 downto m => a('left)); para signed
+
+		--Inicializamos as matrizes de interconexao Cij e Pij:
+		--Cij(i, 0) = 0
+		initCij: for i in 0 to m+n-1 loop			-- for i in m+n-1 downto 0 loop para signed
+			Cij(i, 0) := '0';
+		end loop initCij;
+
+		--Pij(i, 0) = 0
+		initPij1: for i in 0 to m+n loop			-- for i in m+n downto 0 loop para signed / P(0,0) useless
+			Pij(i, 0) := '0';
+		end loop initPij1;
+
+		--Pij(m, i) = 0
+		initPij2: for i in 1 to m+n-1 loop
+			Pij(m, i) := '0';
+		end loop initPij2;
+
+		--Para inicializar a matriz de interconexao Mij eh necessario utilizar lacos de iteracao:
+		--Mij(i, j) = A(i)B(j)
+		Mijrow: for j in 0 to m+n-1 loop
+			Mijcol: for i in 0 to m+n-1 loop
+				Mij(i,j) := a(i) and b(j);
+			end loop Mijcol;
+		end loop Mijrow;
+
+		--Finalmente, podemos interligar os modulos de processamento.
+		--Pi,j	Pij(i, j) 	0 <= i <= m, 0 <= j <= m+n
+		Pijrow: for j in 0 to m+n-1 loop
+			Pijcol: for i in 0 to m+n-1 loop
+				Pij(i,j+1) := Pij(i+1,j) xor Mij(i,j) xor Cij(i,j);
+				Cij(i,j+1) := (Pij(i+1,j) and (Mij(i,j) or Cij(i,j))) or (Mij(i,j) and Cij(i,j));
+			end loop Pijcol;
+		end loop Pijrow;
+
+		--O resultado final p sera igual a:
+		--p(i) = Pij(0, i+1)		0 <= i <= m+n-1
+		initPi: for i in 0 to m+n-1 loop
+			p(i) := Pij(0,i+1);
+		end loop initPi;
+			
+		--RESULT(i) = p(0, i+1)		i in (p'range)
+		genR: for i in RESULT'range loop
+			RESULT(i) := p(i-2*RESULT'right);
+		end loop genR;
+
+		return RESULT;
+	end MULT_FIXED;
 --------------------------------------------------------------
 	--Retorna a conversao de inteiro para ponto fixo
 
 	FUNCTION to_fixed (arg_L: INTEGER; max_range: fixed_range := MAX_IND;
 			min_range: fixed_range := 0) RETURN FIXED IS
-		VARIABLE ponto_fixo : FIXED;
+		VARIABLE ponto_fixo : FIXED(max_range DOWNTO min_range);
 		VARIABLE arg_L_v: INTEGER := arg_L;
 	BEGIN
 		
@@ -199,8 +211,7 @@ PACKAGE BODY fixed_package IS
 	--Retorna a conversao de ponto fixo para inteiro
 
 	FUNCTION to_integer (arg_L: fixed) RETURN INTEGER IS
-			VARIABLE inteiro : INTEGER;
-			VARIABLE ponto_fixo : FIXED;
+		VARIABLE inteiro : INTEGER;
 	BEGIN
 			
 		-- Loop principal para somar os valores da parte inteira do ARRAY
@@ -214,7 +225,7 @@ PACKAGE BODY fixed_package IS
 
 		-- Condicao para colocar o sinal do valor inteiro
 		
-		IF ponto_fixo(arg_L'HIGH) = '1' THEN
+		IF arg_L(arg_L'HIGH) = '1' THEN
 			inteiro := (-1)*inteiro;
 		END IF;
 				
@@ -224,7 +235,7 @@ PACKAGE BODY fixed_package IS
 	-- Retorna a soma de dois pontos fixos
 
 	FUNCTION "+"(arg_L, arg_R: fixed) RETURN fixed IS
-		VARIABLE soma : FIXED;
+		VARIABLE soma : FIXED(MAX_IND DOWNTO MIN_IND);
 	BEGIN
 		soma := ADD_SUB_FIXED(arg_L, arg_R, '0');
 		RETURN soma;
@@ -233,7 +244,12 @@ PACKAGE BODY fixed_package IS
 	-- Retorna a subtracao de dois pontos fixos
 
 	FUNCTION "-"(arg_L, arg_R: fixed) RETURN fixed IS
-		VARIABLE subtracao, complemento, um, maior : FIXED;
+		
+		CONSTANT ind_soma_left : INTEGER := MAXIMUM(arg_L'LEFT,arg_R'LEFT); --O indice da esquerda na soma será o maior
+		CONSTANT ind_soma_right : INTEGER := MAXIMUM(arg_L'RIGHT,arg_R'RIGHT);--O indice da direita na soma será o menor
+		
+		VARIABLE subtracao, complemento, um, maior : FIXED(ind_soma_left DOWNTO ind_soma_right);
+
 	BEGIN -- Soma por complemento de 2
 		IF arg_L'HIGH <= arg_R'HIGH THEN -- Busca o complemento de 2 do menor vetor
 			maior := arg_R;
@@ -252,52 +268,39 @@ PACKAGE BODY fixed_package IS
 	-- Retorna ponto fixo na soma de ponto fixo e inteiro
 
 	FUNCTION "+"(arg_L: FIXED; arg_R: INTEGER) RETURN FIXED IS
-		VARIABLE soma,arg_R_fixed : FIXED;
 	BEGIN
-		arg_R_fixed := to_fixed(arg_R, arg_L'HIGH, arg_L'LOW);
-		soma := arg_L + arg_R_fixed;
-		RETURN soma;
+		RETURN arg_L + to_fixed(arg_R, arg_L'HIGH, arg_L'LOW);
 	END "+";
 --------------------------------------------------------------
 	-- Retorna ponto fixo na soma de inteiro e ponto fixo
 	
 	FUNCTION "+"(arg_L: INTEGER; arg_R: FIXED) RETURN FIXED IS
-		VARIABLE soma,arg_L_fixed : FIXED;
-		VARIABLE inteiro : INTEGER;
 	BEGIN
-		arg_L_fixed := to_fixed(arg_L, arg_R'HIGH, arg_R'LOW);
-		soma := arg_L_fixed + arg_R;
-		RETURN soma;
+		RETURN to_fixed(arg_L, arg_R'HIGH, arg_R'LOW) + arg_R;
 	END "+";
 --------------------------------------------------------------
 	-- Retorna ponto fixo na subtracao de ponto fixo e inteiro
 
 	FUNCTION "-"(arg_L: FIXED; arg_R: INTEGER) RETURN FIXED IS
-		VARIABLE subtracao,arg_R_fixed : FIXED;
 	BEGIN
-		arg_R_fixed := to_fixed(arg_R, arg_L'HIGH, arg_L'LOW);
-		subtracao := arg_L - arg_R_fixed;
-		RETURN subtracao;
+		RETURN arg_L - to_fixed(arg_R, arg_L'HIGH, arg_L'LOW);
 	END "-";
 --------------------------------------------------------------
 	-- Retorna ponto fixo na subtracao de inteiro e ponto fixo
 	
 	FUNCTION "-"(arg_L: INTEGER; arg_R: FIXED) RETURN FIXED IS
-		VARIABLE subtracao,arg_L_fixed : FIXED;
 	BEGIN
-		arg_L_fixed := to_fixed(arg_L, arg_R'HIGH, arg_R'LOW);
-		subtracao := arg_L_fixed - arg_R;
-		RETURN subtracao;
+		RETURN to_fixed(arg_L, arg_R'HIGH, arg_R'LOW) - arg_R;
 	END "-";
 --------------------------------------------------------------
-	-- Retorna a multiplicação de dois pontos fixos
+	-- Retorna a multiplicaÃ§Ã£o de dois pontos fixos
 
 	FUNCTION "*"(arg_L, arg_R: FIXED) RETURN FIXED IS                        
 
-		VARIABLE arg_L_ext : FIXED (2*arg_L'length-2 DOWNTO 0);   --Cria um vetor extendido     
-		VARIABLE arg_R_ext : FIXED (2*arg_R'length-2 DOWNTO 0);   --Cria um vetor extendido 
+		VARIABLE arg_L_ext : FIXED(2*arg_L'length-2 DOWNTO 0);   --Cria um vetor extendido     
+		VARIABLE arg_R_ext : FIXED(2*arg_R'length-2 DOWNTO 0);   --Cria um vetor extendido 
 		
-		VARIABLE Resultado,Mult,Comp1,Comp2,Comp3 : FIXED;
+		VARIABLE Resultado,Mult,Comp1,Comp2,Comp3 : FIXED(2*arg_L'length-2 DOWNTO 0);
 		VARIABLE zero : FIXED(2*arg_L'length-2 DOWNTO 0);
 		
 	BEGIN
@@ -311,14 +314,14 @@ PACKAGE BODY fixed_package IS
 			Comp1 := COMP1_FIXED(arg_R_ext);
 			Comp2 := ADD_SUB_FIXED(zero, Comp1, '1');     --faz o complemento de 2
 			Mult := MULT_FIXED(arg_L_ext,Comp2);
-			Resultado := ADD_SUB_FIXED(zero, Mult, '1');	--faz o complemento de 2 da multiplicação para obter o resultado final
+			Resultado := ADD_SUB_FIXED(zero, Mult, '1');	--faz o complemento de 2 da multiplicaÃ§Ã£o para obter o resultado final
 		
 		ELSIF arg_L(arg_L'high)= '1' AND arg_R(arg_R'high) = '0' THEN   --argL > 0 e arg_R < 0
 		
 			Comp1 := COMP1_FIXED(arg_L_ext);
 			Comp2 := ADD_SUB_FIXED(zero, Comp1, '1');     --faz o complemento de 2
 			Mult := MULT_FIXED(arg_R_ext,Comp2);
-			Resultado := ADD_SUB_FIXED(zero, Mult, '1'); --faz o complemento de 2 da multiplicação para obter o resultado final
+			Resultado := ADD_SUB_FIXED(zero, Mult, '1'); --faz o complemento de 2 da multiplicaÃ§Ã£o para obter o resultado final
 		
 		ELSIF arg_L(arg_L'high)= '1' AND arg_R(arg_R'high) = '1' THEN   --argL < 0 e arg_R < 0
 		
@@ -339,57 +342,29 @@ PACKAGE BODY fixed_package IS
 --------------------------------------------------------------
 	-- Transforma real em fixed
 
-	FUNCTION to_fixed(arg_L:REAL;max_range,min_range:fixed_range) RETURN FIXED IS
-
-		VARIABLE arg_fixed : FIXED;
-		VARIABLE arg_int : INTEGER;
-		VARIABLE arg_dec : INTEGER;
-
+	FUNCTION to_fixed (arg_L: real; max_range, min_range: fixed_range) RETURN FIXED IS
+		CONSTANT RESULT0: FIXED(max_range downto min_range) := (others => '0');
+		CONSTANT RESULTp1: FIXED(max_range downto min_range) := ("0111111111111111");
+		CONSTANT RESULTm1: FIXED(max_range downto min_range) := ("1000000000000000");
+		VARIABLE RESULT: FIXED(max_range downto min_range) := (others => '0');
+		VARIABLE tmp1: INTEGER := 0;
 	BEGIN
-		
 		IF abs(arg_L) < 2.0**(min_range) THEN
-			arg_fixed := (OTHERS => '0');
-
-		ELSIF arg_L >= Real(2**(max_range)) THEN
-			arg_fixed := ((max_range-1)=>'0', OTHERS => '1');
-
-		ELSIF arg_L <= Real(-2**(max_range)) THEN
-			arg_fixed := ((max_range-1)=>'1', OTHERS => '0');
-
-		ELSIF Real(2**(min_range)) <= abs(arg_L) THEN
-
-			arg_int := INTEGER(arg_L) mod 1;
-			arg_dec := INTEGER(arg_L) - arg_int;
-
-			inteiro:FOR i IN max_range'left-1 DOWNTO 0 LOOP
-				IF arg_int mod (2**i) >= 0 THEN
-					arg_fixed(i) := '1';
-					arg_int := arg_int - 2**i;
-				ELSE
-					arg_fixed(i) := '0';
-				END IF;
-			END LOOP inteiro;
-
-
-			decimal:FOR i IN 0 TO min_range'right LOOP
-				IF arg_dec mod (2**i) >= 0 THEN  --AND arg_dec > 0.00001
-					arg_fixed(i) := '1';
-					arg_dec := arg_dec - 2**i;
-				ELSE
-					arg_fixed(i) := '0';
-				END IF;
-			END LOOP decimal;
+			--report "Too small." severity WARNING;
+			RETURN RESULT0;
 		END IF;
-
-		IF arg_L > 0.0 THEN
-			arg_fixed(max_range) := '0';
-
-		ELSE
-			arg_fixed(max_range) := '1';
+		IF abs(arg_L) >= 2.0**(max_range) THEN
+			--report "Too big." severity WARNING;
+			RETURN RESULTp1;
 		END IF;
-
-		RETURN arg_fixed;
-	END to_fixed;
+		IF abs(arg_L) <= -2.0**(max_range) THEN
+			--report "Too bing." severity WARNING;
+			RETURN RESULTm1;
+		END IF;
+		tmp1 := INTEGER(arg_L*2.0**(-min_range));
+		RESULT := to_fixed(tmp1);
+		RETURN RESULT;
+	END FUNCTION;
 
 --------------------------------------------------------------
 	-- Transforma fixed em real
@@ -398,12 +373,12 @@ PACKAGE BODY fixed_package IS
 
 		VARIABLE arg_real : REAL := 0.0;
 
-		VARIABLE arg_L_fim,Comp1 : FIXED;
+		VARIABLE arg_L_fim,Comp1 : FIXED(arg_L'RANGE);
 		VARIABLE zero : FIXED(arg_L'LENGTH DOWNTO 0);
 
 	BEGIN
 
-		IF arg_L(arg_L'HIGH) = '1' THEN	--Número é negativo
+		IF arg_L(arg_L'HIGH) = '1' THEN	--NÃºmero Ã© negativo
 
 			Comp1 := COMP1_FIXED(arg_L);	--faz o complemento de 1
 			arg_L_fim := ADD_SUB_FIXED(zero, Comp1, '1');	--faz o complemento de 2
@@ -414,7 +389,7 @@ PACKAGE BODY fixed_package IS
 			END IF;
 			END LOOP;
 
-		ELSE		--Sem bit de sinal, entao é positivo
+		ELSE		--Sem bit de sinal, entao Ã© positivo
 
 			FOR i IN 0 TO arg_L'LENGTH-1 LOOP
 			IF (arg_L(i) = '1') THEN
@@ -430,59 +405,41 @@ PACKAGE BODY fixed_package IS
 	-- Retorna ponto fixo na soma de ponto fixo e real
 
 	FUNCTION "+"(arg_L: FIXED; arg_R: REAL) RETURN FIXED IS
-		VARIABLE soma, arg_R_fixed : FIXED;
 	BEGIN
-		arg_R_fixed := to_fixed(arg_R, arg_L'HIGH, arg_L'LOW);
-		soma := arg_L + arg_R_fixed;
-		RETURN soma;
+		RETURN arg_L + to_fixed(arg_R, arg_L'HIGH, arg_L'LOW);
 	END "+";
 --------------------------------------------------------------
 	-- Retorna ponto fixo na soma de real e ponto fixo
 	
 	FUNCTION "+"(arg_L: REAL; arg_R: FIXED) RETURN FIXED IS
-		VARIABLE soma, arg_L_fixed : FIXED;
 	BEGIN
-		arg_L_fixed := to_fixed(arg_L, arg_R'HIGH, arg_R'LOW);
-		soma := arg_L_fixed + arg_R;
-		RETURN soma;
+		RETURN to_fixed(arg_L, arg_R'HIGH, arg_R'LOW) + arg_R;
 	END "+";
 --------------------------------------------------------------
 	-- Retorna ponto fixo na subtracao de ponto fixo e real
 
 	FUNCTION "-"(arg_L: FIXED; arg_R: REAL) RETURN FIXED IS
-		VARIABLE subtracao, arg_R_fixed : FIXED;
 	BEGIN
-		arg_R_fixed := to_fixed(arg_R, arg_L'HIGH, arg_L'LOW);
-		subtracao := arg_L - arg_R_fixed;
-		RETURN subtracao;
+		RETURN arg_L - to_fixed(arg_R, arg_L'HIGH, arg_L'LOW);
 	END "-";
 --------------------------------------------------------------
 	-- Retorna ponto fixo na subtracao de real e ponto fixo
 	
 	FUNCTION "-"(arg_L: REAL; arg_R: FIXED) RETURN FIXED IS
-		VARIABLE subtracao, arg_L_fixed : FIXED;
 	BEGIN
-		arg_L_fixed := to_fixed(arg_L, arg_R'HIGH, arg_R'LOW);
-		subtracao := arg_L_fixed - arg_R;
-		RETURN subtracao;
+		RETURN to_fixed(arg_L, arg_R'HIGH, arg_R'LOW) - arg_R;
 	END "-";
 --------------------------------------------------------------
 	-- Retorna ponto fixo na multiplicacao de ponto fixo e real			   
 	FUNCTION "*"(arg_L: fixed; arg_R: real) RETURN FIXED IS
-		VARIABLE produto, arg_R_fixed : FIXED;
 	BEGIN
-		arg_R_fixed := to_fixed(arg_R, arg_L'HIGH, arg_L'LOW);
-		produto := MULT_FIXED(arg_L,arg_R_fixed);
-		RETURN produto;
+		RETURN MULT_FIXED(arg_L,to_fixed(arg_R, arg_L'HIGH, arg_L'LOW));
 	END "*";
 --------------------------------------------------------------
 	-- Retorna ponto fixo na multiplicacao de real e ponto fixo
 	FUNCTION "*"(arg_L: real; arg_R: fixed) RETURN FIXED IS
-		VARIABLE produto, arg_L_fixed : FIXED;
 	BEGIN
-		arg_L_fixed := to_fixed(arg_L, arg_R'HIGH, arg_R'LOW);
-		produto := MULT_FIXED(arg_L_fixed,arg_R);
-		RETURN produto;
+		RETURN MULT_FIXED(to_fixed(arg_L, arg_R'HIGH, arg_R'LOW),arg_R);
 	END "*";
 --------------------------------------------------------------
 END fixed_package;
